96bf9f0a6de5acc3ef178ebb5f0f01618f53d1bd
Yondive <yondive@gmail.com>
2014-11-01 00:33:42 +0600

    test

--- core/src/ru/yondive/colorjumps/MainMenu.java
+++ core/src/ru/yondive/colorjumps/MainMenu.java
@@ -27,7 +27,7 @@ public class MainMenu implements Screen {
 	
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(0, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		stage.act(delta);
